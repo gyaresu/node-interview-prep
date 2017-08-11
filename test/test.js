12bb9f6955dfe51d59e90edb2a54f73998bb3e75
Gareth <me@gareth.codes>
2017-08-11 02:47:26 -0700

    ok, maybe a few mocha and assert tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 var assert = require('assert')
+var obj = {a:"b"}
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+})
+
+describe('Object', function () {
+  describe('#toString()', function() {
+    it('should be a string', function() {
+      assert.equal('b', obj.a.toString()) 
     })
   })
 })
