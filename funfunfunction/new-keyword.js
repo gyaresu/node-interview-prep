2632a74cf8a240fa3304e153b3cf7248c6e2c500
Gareth <me@gareth.codes>
2017-08-22 18:48:23 -0700

    new

---
+++ funfunfunction/new-keyword.js
@@ -0,0 +1,5 @@
+function Person (saying) {
+  this.saying = saying
+}
+
+Per
--- funfunfunction/prototype-basics.js
+++ funfunfunction/prototype-basics.js
@@ -1,13 +1,21 @@
 function talk () {
-  console.log(this) // { talk: [Function: talk] } /// { sound: 'meow!' }
-  console.log(this.sound) // undefined // meow!
+  console.log(this) // { talk: [Function: talk] } /// { wooble: true, sound: 'meow!' }
+  console.log(this.sound) // undefined /// meow!
 }
 let animal = {
   talk
 }
 let cat = {
   wooble: true,
-  sound: 'meow!'
+  sound: 'meow!',
+  colour: 'brown',
+  // talk: function () {
+  //   console.log(this.colour) // brown
+  //   console.log(this.wooble) // true
+  // }
+  talk: () => {
+    console.log(this.colour) // undefined
+  }
 }
 Object.setPrototypeOf(cat, animal)
 // animal.talk()
