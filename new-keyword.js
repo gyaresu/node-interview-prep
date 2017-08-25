1e41cae86dad1f089a80aef597d5c501b2ff3900
Gareth <me@gareth.codes>
2017-08-24 17:37:30 -0700

    swap computers

--- new-keyword.js
+++ new-keyword.js
@@ -1,11 +1,29 @@
-const  {
-  init: function (obj) {
-    this.say = obj.phrase
-  }
+// const  {
+//   init: function (obj) {
+//     this.say = obj.phrase
+//   }
+// }
+
+// var bob = new Person('wooble') // new
+
+// bob.init({ 'phrase': 'I like chips!'})
+
+// console.log(bob)
+
+function personFn (name, age) {
+  let personObj = {}
+  personObj.name = name
+  personObj.age = age
+
+  return personObj
+}
+let bob = personFn('alex', 42)
+console.log(bob, typeof bob)
+
+function PersonConstructor (name, age) {
+  this.name = name
+  this.age = age
 }
 
-var bob = new Person('wooble') // new
-
-bob.init({ 'phrase': 'I like chips!'})
-
-console.log(bob)
+let alex = new PersonConstructor('Alex', 30)
+console.log(alex, typeof alex)
